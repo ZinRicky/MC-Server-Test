e246c9b78b3cac42b5d30b8a2228eae76c7d6e75
ZinRicky <richyz99@420blaze.it>
2020-03-01 00:58:50 +0100

    SES

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# James Chambers - December 28th 2019
+# James Chambers - December 28th 2019 ses
 # More information at https://jamesachambers.com/raspberry-pi-minecraft-server-script-with-startup-service-1-13/
 # GitHub Repository: https://github.com/TheRemote/RaspberryPiMinecraft
 # Minecraft Server startup script using screen
